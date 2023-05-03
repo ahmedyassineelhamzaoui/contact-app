c5d08ae1a67323ccf486131deea0c8cd0e3b5cf8
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-05-03 16:34:34 +0100

    add tow component home and add to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
