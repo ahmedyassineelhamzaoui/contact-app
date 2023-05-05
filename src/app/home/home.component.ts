a9a0eb286549db16c8567a2005a54e9458294155
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-05-05 15:14:02 +0100

    create a method to show details of contact

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ContactService } from '../contact.service';
+import { Contact } from '../contact';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,8 @@ import { ContactService } from '../contact.service';
 export class HomeComponent {
   Date : Date = new Date();
   contacts: any[] = [];
-
+  contactToEdit : Contact =new Contact(); 
+  editIndex : number | undefined;
   constructor(private contactService: ContactService) { }
 
   ngOnInit() {
@@ -23,4 +25,20 @@ export class HomeComponent {
     console.log('contact is deleted successfuly');
   }
 
+  editContact(index : number){
+    const box = document.querySelector('#editContactModal') as HTMLDivElement;
+    box.classList.add('flex');
+    box.classList.remove('hidden');
+    this.contactToEdit.id = this.contacts[index].id; 
+    this.contactToEdit.Nom = this.contacts[index].Nom; 
+    this.contactToEdit.Prenom = this.contacts[index].Prenom; 
+    this.contactToEdit.Date_naissance = this.contacts[index].Date_naissance; 
+    this.contactToEdit.Tel = this.contacts[index].Tel; 
+    this.editIndex = index;
+  }
+  hidContacteModal(){
+    const box = document.querySelector('#editContactModal') as HTMLDivElement;
+    box.classList.add('hidden');
+    box.classList.remove('flex');
+  }
 }
